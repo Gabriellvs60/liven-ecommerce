224f8a54782fc6a187beb558eecac61bdbdeb04c
gabriel.alves <gabriel.alves@brainny.cc>
2022-01-29 12:31:41 -0300

    :sparkles: Storybook configured

---
+++ src/components/atoms/Card/Card.stories.tsx
@@ -0,0 +1,10 @@
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Card from './index';
+
+export default {
+  title: 'atoms/Card',
+  component: Card,
+} as ComponentMeta<typeof Card>;
+
+export const Primary: ComponentStory<typeof Card> = () => <Card width="100px" height="200px">Card example</Card>;
