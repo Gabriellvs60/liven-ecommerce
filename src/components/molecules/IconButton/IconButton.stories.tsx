1f0c54e27b0947905b73d5df23aef26ad3e83e63
gabriel.alves <gabriel.alves@brainny.cc>
2022-02-05 19:14:41 -0300

    :art: Removing any type from stories

--- src/components/molecules/IconButton/IconButton.stories.tsx
+++ src/components/molecules/IconButton/IconButton.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { HiOutlineShoppingBag } from "react-icons/hi";
 import IconButton from "./index";
+import IconButtonProps from "./index";
 
 export default {
   title: "molecules/IconButton",
@@ -11,30 +12,30 @@ export default {
   },
 } as ComponentMeta<typeof IconButton>;
 
-export const Default: ComponentStory<typeof IconButton> = (args: any) => (
+export const Default: ComponentStory<typeof IconButtonProps> = (args) => (
   <IconButton {...args}>
     <HiOutlineShoppingBag fontSize="20px" />
   </IconButton>
 );
 
-export const WithBadgeCount: ComponentStory<typeof IconButton> = (
-  args: any
+export const WithBadgeCount: ComponentStory<typeof IconButtonProps> = (
+  args
 ) => (
   <IconButton {...args} badgeCount={2}>
     <HiOutlineShoppingBag fontSize="20px" />
   </IconButton>
 );
 
-export const WithBackgroundColorHover: ComponentStory<typeof IconButton> = (
-  args: any
+export const WithBackgroundColorHover: ComponentStory<typeof IconButtonProps> = (
+  args
 ) => (
   <IconButton {...args} bgColorHover="#838383">
     <HiOutlineShoppingBag fontSize="20px" />
   </IconButton>
 );
 
-export const WithoutBackground: ComponentStory<typeof IconButton> = (
-  args: any
+export const WithoutBackground: ComponentStory<typeof IconButtonProps> = (
+  args
 ) => (
   <IconButton {...args} withBackground={false}>
     <HiOutlineShoppingBag fontSize="20px" />
